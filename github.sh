c7e66214233e56206bfb2e707e03d57e4fc14def
Morgan Goose <morgan.goose@gmail.com>
2010-09-29 14:18:03 -0400

    had to change up flags to get out of infinate loop

--- github.sh
+++ github.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-#post-push
-if [[ $HG_ARGS =~ "push bitbucket" ]]
+# This script takes in the args that a hg push is given and checks fo the paths
+# that I've defined in my hg repos for either a push to bitbucket or github,
+# and then does the other, so that regardless of which of these sites I push to
+# the other also get pushed to.
+
+if [[ $HG_ARGS = "push bitbucket" ]]
 then 
-    hg push github
+    #post-push to bitbucket
+    echo 
+    hg push github --quiet
+    echo 
+
+elif [[ $HG_ARGS = "push github" ]]
+then 
+    #post-push to github
+    hg push bitbucket --quiet
 fi
